0a73b521d00b934d511d791a180b6cc4209b9be2
Ganna Kaplun <tangriel@hotmail.com>
2014-05-11 17:30:51 +0300

    makeCacheMatrix, cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation.
+## Thus, there may be some benefit to caching the inverse of a matrix 
+## rather than computing it repeatedly. 
+## These functions cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      
+      inv <- NULL
+      
+      ## Set/get methods for the matrix
+      
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      
+      ## Set/get methods for the matrix inverse
+      
+      setinv <- function(solve) inv <<- solve
+      getinv <- function() inv
+      
+      ## Returning the list of set/get methods
+      
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then it retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+      inv <- x$getinv()
+      
+      ## Check if the inverse has already been calculated,
+      ## if so, get it from the cache and skip the computation
+      
+      if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+      }
+      
+      ## Compute a matrix that is the inverse of 'x'
+      
+      data <- x$get()
+      
+      inv <- solve(data, ...)
+      x$setinv(inv)
+      
+      ## Return a matrix that is the inverse of 'x'
+      
+      inv
 }
